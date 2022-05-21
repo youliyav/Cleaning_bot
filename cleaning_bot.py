f62beb94f3588a15accda8933c17cd463f0a44d1
Youliya Bratishko <youliyav@gmail.com>
2022-05-22 00:12:27 +0300

    Fixed a bug

--- cleaning_bot.py
+++ cleaning_bot.py
@@ -101,10 +101,5 @@ async def all_msg_handler(message: types.Message):
 
 
 if __name__ == '__main__':
-
     executor.start_polling(dp, skip_updates=True)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
 
